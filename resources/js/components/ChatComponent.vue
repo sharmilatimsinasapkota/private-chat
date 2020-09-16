ca9931364319683be4802445fd44b9ccb1d97038
vagrant <vagrant@homestead>
2020-09-16 13:49:47 +0000

    added message component

--- resources/js/components/ChatComponent.vue
+++ resources/js/components/ChatComponent.vue
@@ -3,12 +3,16 @@
         <div class="row ">
             <div class="col-md-3">
                 <div class="card   card-default">
-                    <div class="card-header">Private Chat</div>
+                    <div class="card-header">Private Chat App</div>
                             <ul class="list-group">
                                 <li class="list-group-item">Cras justo odio</li>
                             </ul>
                 </div>
             </div>
+            <div class="col-md-9">
+                <message-component></message-component>
+            </div>
+
         </div>
     </div>
 </template>
---
+++ resources/js/components/MessageComponent.vue
@@ -0,0 +1,27 @@
+<template>
+                <div class="card   card-default chat-box" >
+                    <div class="card-header">Chats
+                    </div>
+                    <div class="card-body">
+                         Hey how are you
+                    </div>
+                    <form class="card-footer">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Write your message here">
+                        </div>
+                    </form>
+                </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style>
+.chat-box{  
+        height: 400px;
+}
+</style>
